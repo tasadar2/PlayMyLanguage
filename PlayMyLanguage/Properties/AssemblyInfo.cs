78cd6a2d4ab1d3dc9450472ab0c70b25a3f1682b
tasadar2 <tassadar@wowway.com>
2018-01-28 00:35:36 -0500

    correcting log bug

--- PlayMyLanguage/Properties/AssemblyInfo.cs
+++ PlayMyLanguage/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.127.1")]
+[assembly: AssemblyVersion("2018.127.2")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
